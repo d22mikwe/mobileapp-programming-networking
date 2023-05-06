79f594fd2185755fd724fb8475829504a9818a3e
Micke <micke.welander@live.se>
2023-05-06 21:49:02 +0200

    Changed color of text

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -21,19 +21,12 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
-
-
-    ArrayList<MountainItem> items = new ArrayList<>(Arrays.asList(
-            new MountainItem("Matterhorn"),
-            new MountainItem("Mont Blanc"),
-            new MountainItem("Denali")
-    ));
+    ArrayList<MountainItem> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         new JsonFile(this, this).execute(JSON_FILE);
         new JsonTask(this).execute(JSON_URL);
 
@@ -57,7 +50,6 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         RecyclerView view = findViewById(R.id.recycler_view);
         view.setAdapter(adapter);
         view.setLayoutManager(new LinearLayoutManager(this));
-
 
     }
 
--- app/src/main/java/com/example/networking/MountainItem.java
+++ app/src/main/java/com/example/networking/MountainItem.java
@@ -2,15 +2,10 @@ package com.example.networking;
 
 @SuppressWarnings("WeakerAccess")
 public class MountainItem {
-
-
-
     private String name;
     public MountainItem (String name) {
         this.name = name;
     }
-
-
     public String getName() {
         return name;
     }
