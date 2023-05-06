d079c2e32cbf101c25875fd3ca2477ab602253e0
Micke <micke.welander@live.se>
2023-05-06 21:05:16 +0200

    Accessed internet for app, created item list file and Mountain Item java class. Configured MainActivity

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,14 +2,32 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
+
+
+    ArrayList<MountainItem> items = new ArrayList<>(Arrays.asList(
+            new MountainItem("Matterhorn"),
+            new MountainItem("Mont Blanc"),
+            new MountainItem("Denali")
+    ));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +35,30 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
 
         new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(this).execute(JSON_URL);
+
     }
 
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
+
+        Gson newGson = new Gson();
+        Type newType = new TypeToken<List<MountainItem>>() {}.getType();
+        items = newGson.fromJson(json, newType);
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(MountainItem item) {
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView view = findViewById(R.id.recycler_view);
+        view.setAdapter(adapter);
+        view.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter.notifyDataSetChanged();
     }
 
 }
---
+++ app/src/main/java/com/example/networking/MountainItem.java
@@ -0,0 +1,16 @@
+package com.example.networking;
+
+@SuppressWarnings("WeakerAccess")
+public class MountainItem {
+
+
+    private String title;
+    public MountainItem (String title) {
+        this.title = title;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+}
